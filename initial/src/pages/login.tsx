5cf6a3e57945ef101ed1a13e0a8da697f9a87dd3
iltrds <aidanmccarthy01@gmail.com>
2026-03-01 22:02:23 -0500

    Added CIS4010

--- initial/src/pages/login.tsx
+++ initial/src/pages/login.tsx
@@ -2,10 +2,14 @@ import Login, { Render } from 'react-login-page';
 
 function LoginPage() {
   return (
+
+    
+
     <Login>
       <Render>
         {({ fields, buttons }) => (
           <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column', height: '100vh', width: '175vh'}}>
+            <h1>CIS 4010 Survey System</h1>
             <h2>Login</h2>
             <label>{fields.username}</label>
             <label>{fields.password}</label>
