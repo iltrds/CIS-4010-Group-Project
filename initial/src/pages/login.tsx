e11f109c76ec7f0b85b33fff320ffe253534817e
iltrds <aidanmccarthy01@gmail.com>
2026-03-01 22:00:37 -0500

    Login Screen

---
+++ initial/src/pages/login.tsx
@@ -0,0 +1,23 @@
+import Login, { Render } from 'react-login-page';
+
+function LoginPage() {
+  return (
+    <Login>
+      <Render>
+        {({ fields, buttons }) => (
+          <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column', height: '100vh', width: '175vh'}}>
+            <h2>Login</h2>
+            <label>{fields.username}</label>
+            <label>{fields.password}</label>
+            <div>{buttons.submit}</div>
+          </div>
+        )}
+      </Render>
+      <Login.Input keyname="username" placeholder="Enter username" />
+      <Login.Input keyname="password" placeholder="Enter password" type="password" />
+      <Login.Button keyname="submit" type="submit">Login</Login.Button>
+    </Login>
+  );
+}
+
+export default LoginPage;
